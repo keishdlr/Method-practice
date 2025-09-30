bb3f7584f53df1073199a4f51bff30c4a36faddf
keisha De La Rosa <keishdlr@gmail.com>
2025-09-30 09:55:55 -0400

    added format class

---
+++ src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Format {
+
+    public static void main(String[] args) {
+
+        String last = "De La Rosa"; // Variable for last name
+        String first = "Keisha"; // Variable for first name
+
+        String fullName = formatName(first, last);   // Call the method
+        System.out.println(fullName); // Print full name
+    }
+
+    public static String formatName(String first, String last) {
+
+        return (last + "," + first);
+    }
+}
